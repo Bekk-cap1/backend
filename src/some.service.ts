bf0a7e92b1134d7dfd8332a94ac42ad07e298526
Bekk-cap1 <masadbek2006@gmail.com>
2026-04-10 02:23:55 +0500

    fix(ci): remove debug service and untrack tmp cookies file

--- src/some.service.ts
+++
@@ -1,4 +0,0 @@
-import { getRequestContext } from './infrastructure/request-context/request-context';
-
-const ctx = getRequestContext();
-console.log(ctx.requestId, ctx.ip, ctx.userAgent, ctx.actorId, ctx.actorRole);
